e7968470060c094ece8c95cd6baee2074c9314eb
Nitesh065 <niteshwadhwani605@gmail.com>
2021-10-31 22:42:38 +0530

    read data from csv and made simple pdf(31-10-21)

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import codecs
-
 from fastapi import FastAPI, File, UploadFile,Request
-import pandas as pd
-import numpy as n
-from pathlib import Path
-import shutil
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import A4, inch, landscape, legal, letter
 import csv
-import os
+import  os
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Image, Spacer, PageBreak
+from reportlab.lib.units import cm, inch
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import *
+from reportlab.platypus import *
 from reportlab.lib.styles import getSampleStyleSheet
-import uvicorn
+
 
 app = FastAPI()
 
@@ -26,70 +22,77 @@ def index(request:Request):
     return templates.TemplateResponse("home.html", {"request":request})
 
 
- # at last, the bottom of the file/module
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="127.0.0.1", port=5049)
-# app.get("/",response_class=HTMLResponse)
-# def index(request:Request):
-#     return {"message":"HElloworld"}
-#     return """<html>
-#   <head>
-#     <title>Report Generator</title>
-#   </head>
-#   <body>
-#     <h1>Upload .csv Fiel</h1>
-#     <form action="/submitForm" method="post" enctype="multipart/form-data">
-#       <input type= "file" name="csv_file">
-#       <input type="submit" value="Generate report">
-#     </form>
-#   </body>
-# </html>
-# """
-
-
 @app.post("/submitForm")
 async def upload_file(csv_file:UploadFile = File(...)):
-    #Pdf layout
+    #reading data from csv in a list
+    readFile = csv.reader(codecs.iterdecode(csv_file.file,'utf-8'))
+    dataList = list(readFile)
+    print(readFile)
+
+
+    element = []
+
+    style = getSampleStyleSheet()
+    normalStyle = style["Normal"]
+    #making pdf table
+    #[(starting_column,starting_row), (ending_column, ending_row)]
+
+    all_cells = [(0, 0), (-1, -1)]
+    header = [(0, 0), (-1, 0)]
+    column0 = [(0, 0), (0, -1)]
+    column1 = [(1, 0), (1, -1)]
+    column2 = [(2, 0), (2, -1)]
+    column3 = [(3, 0), (3, -1)]
+    column4 = [(4, 0), (4, -1)]
+    column5 = [(5, 0), (5, -1)]
+    column6 = [(6, 0), (6, -1)]
 
     table_style = TableStyle(
         [
-            ('ALIGN', (1, 1), (-2, -2), 'RIGHT'),
-            ('VALIGN', (0, 0), (0, -1), 'TOP'),
-            ('ALIGN', (0, -1), (-1, -1), 'CENTER'),
-            ('VALIGN', (0, -1), (-1, -1), 'MIDDLE'),
-            ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
-            ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+            ('VALIGN', all_cells[0], all_cells[1], 'TOP'),
+            ('LINEBELOW', header[0], header[1], 1, colors.black),
+            ('ALIGN', column0[0], column0[1], 'LEFT'),
+            ('ALIGN', column1[0], column1[1], 'LEFT'),
+            ('ALIGN', column2[0], column2[1], 'LEFT'),
+            ('ALIGN', column3[0], column3[1], 'RIGHT'),
+            ('ALIGN', column4[0], column4[1], 'LEFT'),
+            ('ALIGN', column5[0], column5[1], 'LEFT'),
+            ('ALIGN', column6[0], column6[1], 'LEFT'),
         ]
     )
-    styles = getSampleStyleSheet()
-    styleNormal = styles['Normal']
 
-    style_config = getSampleStyleSheet()
-    style_config = style_config["BodyText"]
-    style_config.wordWrap = 'CJK'
+    #column widths
+    columnWidht = [
+        3.7 * cm,
+        3.7 * cm,
+        3.7 * cm,
+        3.7 * cm,
+        3.7 * cm,
+        3.7 * cm,
+        6 * cm,
+    ]
+    for index, row in enumerate(dataList):
+
+        for column, value in enumerate(row):
+            try:
+                if column != 6 or index == 0:
+                    dataList[index][column] = value.strip("'[]()")
+                else:
+                    dataList[index][column] = Paragraph(value, style["Normal"])
+            except ValueError:
+                pass
+
+    table = Table(dataList, colWidths=columnWidht)
+    table.setStyle(table_style)
+    #print(table)
+    element.append(table)
+    fileName = cwd + '\\' + 'Demo Report.pdf'
+
+    print(element)
+
+    create_pdf = SimpleDocTemplate(fileName,pagesize=letter,rightMargin=40,leftMargin=40,topMargin=40,bottomMargin=28)
+    create_pdf.build(element)
+    print('Demo Report Generated')
 
 
-    readFile = csv.DictReader(codecs.iterdecode(csv_file.file,'utf-8'))
-    dataList = list(readFile)
-    #print(dataList)
-    headers = dataList[0]
-    #print(headers)
-    for record in range(1,len(dataList)):
-        file_records = dataList[record]
-        print((file_records))
-
-        data = list()
-        nullRecords = list()
-        records = list()
-        headers = list()
-        count = 0
-
-        for line in file_records:
-            if line == "":
-                nullRecords.append(line)
-            else:
-                records.append(line)
-                headers.append(count)
-                data.append([str(headers[count]),str(line)])
-            count += 1
-    return {"fileName":csv_file.filename}
+    return {"Report_Genrated":csv_file.filename}
